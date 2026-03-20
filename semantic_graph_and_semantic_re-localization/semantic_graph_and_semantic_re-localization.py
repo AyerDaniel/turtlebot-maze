0c427e1a9054c39e143946d39e653fab557d88a2
Daniel Ayer <da632@njit.edu>
2026-03-20 16:31:54 -0400

    Enabling AGE per session as per documentation.

--- semantic_graph_and_semantic_re-localization/semantic_graph_and_semantic_re-localization.py
+++ semantic_graph_and_semantic_re-localization/semantic_graph_and_semantic_re-localization.py
@@ -15,6 +15,10 @@ import uuid
 import math
 #import rclpy
 
+from psycopg.rows import dict_row
+import clip
+from PIL import Image
+import torch
 from datetime import datetime, timezone
 
 # Import packages for deserializing Zenoh output from ROS2 sensor_msgs/msg/Image.
@@ -173,136 +177,186 @@ class semantic_pipeline:
             self.session.close()
             print("Monitor stopped.", flush=True)
 
+def detect_objects():
 
-if __name__ == "__main__":
-    #pipeline = semantic_pipeline(table, show_camera=False)
-    #pipeline.run()  <--- Uncomment to run the pipeline.
-    print()
-
-##  Processing Steps For Assignment 3 ##
-'''
-    This section contains scripts I ran to fullfill the requirements of Assignment 3.
-    They are here to consolodate code for evaluation purposes.  
-    Normally I would split them into their own scripts.
-
-'''
-
-## Create embeddings of detections.
-
-# Read embeddings.
-try:
-    from psycopg.rows import dict_row
-    import clip
-    from PIL import Image
-    import torch
-
-    # Set device and load model to create embeddings.
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-
-    with psycopg.connect(
-        f"dbname={dbname} user={user} password={password} host={host} port={port}",
-        row_factory=dict_row
-    ) as conn:
+    '''
         
-        # Create a cursor.  Set returns as dictionaries.
-        cursor = conn.cursor()
+        This function assumes zenoh-router, zenoh-bridge, dectector, demo-world-enhanced containers are running.
 
-        # Prepare query sql.
-        query_sql = """
-            SELECT det_pk, class_name, x1, y1, x2, y2, image_raw FROM detections LEFT JOIN detection_events ON detections.event_id = detection_events.event_id
-        """
+    '''
+    # Instantiate pipeline to utilize Zenoh and such to record detections from ROS2 and Gazebo.
+    pipeline = semantic_pipeline(table, show_camera=False)
+
+    # Run pipeline.
+    pipeline.run()
+
+def create_embeddings():
+
+    '''
+
+        This function assumes the postgres container is running.
+        This container should have the tables holding the detections and detection events.
+
+    '''
+    try:
         
-        # Execute query.
-        detections = cursor.execute(query_sql)
+        # Set device and load model to create embeddings.
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model, preprocess = clip.load("ViT-B/32", device=device)
 
-        # Get all detections.
-        detections = cursor.fetchall()
-
-        # Create embeddings for each row result.  Each row is a detection.
-        for row in detections:
+        with psycopg.connect(
+            f"dbname={dbname} user={user} password={password} host={host} port={port}",
+            row_factory=dict_row
+        ) as conn:
             
-            # Store each returned field as a variable.
-            det_pk = row['det_pk']                                                                                                                                                                      
-            # event_id = row['event_id']
-            # det_id = row['det_id']                                                                                                                                                                      
-            # class_id = row['class_id']
-            class_name = row['class_name']                                                                                                                                                              
-            # confidence = row['confidence']
-            x1 = row['x1']                                                                                                                                                                              
-            y1 = row['y1']                                                                                                                                                                              
-            x2 = row['x2']                                                                                                                                                                              
-            y2 = row['y2']                                                                                                                                                                              
-            # timestamp = row['timestamp']
-            # run_id = row['run_id']
-            # robot_id = row['robot_id']                                                                                                                                                                  
-            # sequence = row['sequence']
-            # time_in_run = row['time_in_run']                                                                                                                                                            
-            # image_frame_id = row['image_frame_id']
-            # image_sha256 = row['image_sha256']                                                                                                                                                          
-            # width = row['width']                                                                                                                                                                        
-            # height = row['height']                                                                                                                                                                      
-            # encoding = row['encoding']                                                                                                                                                                  
-            # x = row['x']                                                                                                                                                                                
-            # y = row['y']    
-            # yaw = row['yaw']
-            # vx = row['vx']                                                                                                                                                                              
-            # vy = row['vy']                                                                                                                                                                              
-            # wz = row['wz']                                                                                                                                                                              
-            # tf_ok = row['tf_ok']                                                                                                                                                                        
-            # t_base_camera = row['t_base_camera']
-            # raw_event = row['raw_event']
-            image_raw = row['image_raw']
+            # Create a cursor. 
+            cursor = conn.cursor()
 
-            # Read in image.  Expected encoding: 'rgb8, bytes: 19437' output from ROS2.
-            nparr = np.frombuffer(image_raw, np.uint8)                                                                                                                                                  
-            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)                                                                                                                                                 
+            # Prepare query sql.
+            query_sql = """
+                SELECT det_pk, class_name, x1, y1, x2, y2, image_raw FROM detections LEFT JOIN detection_events ON detections.event_id = detection_events.event_id
+            """
             
-            # Crop image to bbox coords.
-            cropped = img[int(y1):int(y2), int(x1):int(x2)] 
+            # Execute query.
+            detections = cursor.execute(query_sql)
 
-            # Create PIL input for CLIP.
-            pil_image = Image.fromarray(cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB))
+            # Get all detections.
+            detections = cursor.fetchall()
 
-            # Preprocess for CLIP                                                                                                                                                                   
-            image_tensor = preprocess(pil_image).unsqueeze(0).to(device)                                                                                                                            
+            # Create embeddings for each row result.  Each row is a detection.
+            for row in detections:
+                
+                # Store each returned field as a variable.
+                det_pk = row['det_pk']                                                                                                                                                                      
+                # event_id = row['event_id']
+                # det_id = row['det_id']                                                                                                                                                                      
+                # class_id = row['class_id']
+                class_name = row['class_name']                                                                                                                                                              
+                # confidence = row['confidence']
+                x1 = row['x1']                                                                                                                                                                              
+                y1 = row['y1']                                                                                                                                                                              
+                x2 = row['x2']                                                                                                                                                                              
+                y2 = row['y2']                                                                                                                                                                              
+                # timestamp = row['timestamp']
+                # run_id = row['run_id']
+                # robot_id = row['robot_id']                                                                                                                                                                  
+                # sequence = row['sequence']
+                # time_in_run = row['time_in_run']                                                                                                                                                            
+                # image_frame_id = row['image_frame_id']
+                # image_sha256 = row['image_sha256']                                                                                                                                                          
+                # width = row['width']                                                                                                                                                                        
+                # height = row['height']                                                                                                                                                                      
+                # encoding = row['encoding']                                                                                                                                                                  
+                # x = row['x']                                                                                                                                                                                
+                # y = row['y']    
+                # yaw = row['yaw']
+                # vx = row['vx']                                                                                                                                                                              
+                # vy = row['vy']                                                                                                                                                                              
+                # wz = row['wz']                                                                                                                                                                              
+                # tf_ok = row['tf_ok']                                                                                                                                                                        
+                # t_base_camera = row['t_base_camera']
+                # raw_event = row['raw_event']
+                image_raw = row['image_raw']
+
+                # Read in image.  Expected encoding: 'rgb8, bytes: 19437' output from ROS2.
+                nparr = np.frombuffer(image_raw, np.uint8)                                                                                                                                                  
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)                                                                                                                                                 
+                
+                # Crop image to bbox coords.
+                cropped = img[int(y1):int(y2), int(x1):int(x2)] 
+
+                # Create PIL input for CLIP.
+                pil_image = Image.fromarray(cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB))
+
+                # Preprocess for CLIP                                                                                                                                                                   
+                image_tensor = preprocess(pil_image).unsqueeze(0).to(device)                                                                                                                            
+                
+                # Get embeddings.
+                with torch.no_grad():                                                                                                                                                                   
+                    image_features = model.encode_image(image_tensor)                                                                                                                                   
+                
+                # Create sql to write to new table.
+                write_embeddings_sql = '''
+                    INSERT INTO detection_embeddings (det_pk, model, embedding) VALUES (%s, %s, %s)
+                    ON CONFLICT (det_pk) DO NOTHING
+                '''
+
+                # Move embedding to cpu.
+                image = image_features.cpu()
+
+                # Convert to numpy array.
+                image = image.numpy()
+                
+                # Flatten, is [1, 512] for some reason.
+                image = image.flatten()
+                
+                # Convert to list for psycopg.
+                image = image.tolist()
+
+                try:
+
+                    # Stage write to table.
+                    cursor.execute(write_embeddings_sql, (row['det_pk'], row['class_name'], image))
+
+                    # Commit to db.
+                    conn.commit()
+
+                except Exception as e:
+                    # Report problems.
+                    print(e)
+                
+
+    except Exception as e:
+        print(f"Error inserting data: {e}")
+
+def do_age():
+
+    '''
+
+        This function assumes the detection_embeddings table has been populated appropriately.
+
+    '''
+
+    # Load age.
+    try:
             
-            # Get embeddings.
-            with torch.no_grad():                                                                                                                                                                   
-                image_features = model.encode_image(image_tensor)                                                                                                                                   
+        with psycopg.connect(
+                f"dbname={dbname} user={user} password={password} host={host} port={port}",
+                row_factory=dict_row
+            ) as conn:
             
-            # Create sql to write to new table.
-            write_embeddings_sql = '''
-                INSERT INTO detection_embeddings (det_pk, model, embedding) VALUES (%s, %s, %s)
-                ON CONFLICT (det_pk) DO NOTHING
-            '''
+            # Create cursor.
+            cursor = conn.cursor()
 
-            # Move embedding to cpu.
-            image = image_features.cpu()
-
-            # Convert to numpy array.
-            image = image.numpy()
-            
-            # Flatten, is [1, 512] for some reason.
-            image = image.flatten()
-            
-            # Convert to list for psycopg.
-            image = image.tolist()
-
+            # Load AGE.
             try:
-
-                # Stage write to table.
-                cursor.execute(write_embeddings_sql, (row['det_pk'], row['class_name'], image))
-
-                # Commit to db.
-                conn.commit()
+                
+                cursor.execute("LOAD 'age'")
 
             except Exception as e:
-                # Report problems.
-                print(e)
-            
 
-except Exception as e:
-    print(f"Error inserting data: {e}")
+                print(f"Exception thrown: {e}")
+
+            # Set search path.
+            try:
+
+                cursor.execute(f'SET search_path = ag_catalog, "$user", public')
+            
+            except Exception as e:
+                
+                print(f"Exception thrown: {e}")
+
+    except Exception as e:
+        
+        print(e)
+
+if __name__ == "__main__":
+    
+
+    #detect_objects()
+    #create_embeddings()
+    do_age()
+
+    
 
 
